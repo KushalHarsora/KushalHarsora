f1ee3c9143c95867125162eb591a9b70082faad3
Kushal Harsora <kushalharsora13@gmail.com>
2024-05-11 14:21:16 +0530

    Initial Changes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from "react";
+
+const About = () => {
+    return(
+        <React.Fragment>
+            <div className=" text-center text-red-600 text-xl">
+                About Page
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default About;
