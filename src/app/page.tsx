88bcae5850a5ec57f9a5ca09e6c31742a364ed09
Kushal Harsora <kushalharsora13@gmail.com>
2024-05-18 16:10:18 +0530

    Updated README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Home() {
   return (
     <React.Fragment>
-      <div className=" text-center">
+      <div className=" flex justify-center items-center h-screen w-screen text-2xl font-bold">
         Kushal Harsora
       </div>
     </React.Fragment>
