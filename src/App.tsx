5f24ac8bafa8b4b5e12dc4f35070347644bd2909
Kushal Harsora <kushalharsora13@gmail.com>
2025-04-04 16:02:32 +0530

    fixed model and added scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
 import { Mouse } from 'lucide-react';
 import React from 'react';
 const Model = React.lazy(() => import('./components/custom/Model'));
-import Lenis from 'lenis';
 
-const lenis = new Lenis({
-  autoRaf: true,
-});
-
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
 
 const App = () => {
   return (
     <React.Fragment>
       <React.Suspense fallback={<main className='h-screen w-screen overflow-hidden flex items-center justify-center bg-black text-white text-2xl'>Loading...</main>}>
-        <main className='h-[150vh] w-screen flex flex-col items-center justify-center relative overflow-x-hidden'>
-          <section className='relative z-10 flex flex-col items-center justify-center gap-5 h-[50vh] w-screen text-white bg-black text-7xl max-lg:text-7xl max-md:text-4xl max-md:px-2'>
-            <div className='w-screen h-fit flex flex-col items-center justify-center gap-2'>
-              <span className=' text-orange-500 font-semibold'>NAMASTE</span>
-              <span className=' max-md:text-2xl max-lg:text-6xl'>I am <span className=' font-bold'>KUSHAL HARSORA</span></span>
+        <main className='h-screen w-screen flex flex-col items-center justify-center relative overflow-x-hidden'>
+          <section className='absolute top-0 left-0 z-10 flex flex-col items-center justify-start gap-3 pt-20 h-[150vh] w-screen text-white bg-transparent text-7xl max-lg:text-7xl max-md:text-4xl max-md:px-2'>
+            <div>
+              <span className=' text-7xl max-md:text-6xl'>NAMASTE👋🏻</span>
             </div>
-            <div className='w-screen h-auto flex justify-center items-center'>
-              <span className=' w-fit flex flex-row gap-1 text-white text-base animate-bounce border border-white py-2.5 px-4 rounded-3xl'>
-              Scroll Down <Mouse />
+            <div>
+              <span className='w-fit flex flex-row justify-center items-center gap-1 text-white text-base max-md:text-xs animate-bounce border border-white py-2.5 max-md:py-1.5 px-4 max-md:px-3 rounded-3xl'>
+              Scroll Down <Mouse className=' max-md:w-[20px]' />
               </span>
             </div>
           </section>
-          <Model />
+          <section className='fixed top-0 left-0 z-0 h-screen w-screen flex items-center justify-center bg-black'>
+            <Model />
+          </section>
         </main>
       </React.Suspense>
     </React.Fragment>
--- src/components/custom/Model.tsx
+++ src/components/custom/Model.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera, useGLTF } from '@react-three/drei'
-import { EffectComposer, BrightnessContrast } from '@react-three/postprocessing'
+import { EffectComposer, BrightnessContrast, Bloom, Vignette } from '@react-three/postprocessing'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
@@ -14,23 +14,26 @@ const Model = () => {
 
     const earthRef = React.useRef<THREE.Object3D>(null);
 
-    const initProps = {
-        pos: [-250, 15, 30] as [number, number, number],
-        intensity: 10 as number,
-    }
+    const isMobile = window.innerWidth <= 768;
 
-    Earth.traverse((child) => {
-        if (child instanceof THREE.Mesh) {
+    const initProps = {
+        pos: [-100, 60, 10] as [number, number, number],
+        intensity: !isMobile ? 7.5 : 10 as number,
+    }
+    
+    React.useMemo(() => {
+        Earth.traverse((child) => {
+          if (child instanceof THREE.Mesh) {
             child.material.envMapIntensity = 0.5;
             child.material.needsUpdate = true;
-        }
-    });
+          }
+        });
+    }, [Earth]);
 
     React.useEffect(() => {
         const animate = () => {
             if (earthRef.current) {
                 earthRef.current.rotation.y -= 0.00125;
-                earthRef.current.rotation.z += 0.0005;
             }
 
             requestAnimationFrame(animate);
@@ -41,48 +44,46 @@ const Model = () => {
 
     return (
         <React.Fragment>
-            <section className='h-screen w-screen flex items-center justify-center bg-black'>
-                <Canvas
-                    style={{ backgroundColor: 'black' }}
-                    shadows
-                    gl={{
-                        preserveDrawingBuffer: true,
-                        antialias: true,
-                        alpha: true,
-                        powerPreference: 'low-power',
-                        stencil: false,
-                        depth: true,
-                        failIfMajorPerformanceCaveat: true,
-                        logarithmicDepthBuffer: true,
-                        toneMapping: THREE.ACESFilmicToneMapping,
-                        toneMappingExposure: 1.5,
-                    }}
-                >
-                    <PerspectiveCamera
-                        makeDefault fov={30}
-                        near={0.1}
-                        far={10000}
-                        position={[0, 0, 2]}
-                    />
+            <Canvas
+                style={{ backgroundColor: 'black' }}
+                shadows
+                gl={{
+                    preserveDrawingBuffer: true,
+                    antialias: true,
+                    alpha: true,
+                    powerPreference: 'high-performance',
+                    precision: 'highp',
+                    stencil: false,
+                    depth: true,
+                    failIfMajorPerformanceCaveat: true,
+                    logarithmicDepthBuffer: true,
+                    toneMapping: THREE.ACESFilmicToneMapping,
+                    toneMappingExposure: 1.5,
+                    pixelRatio: window.devicePixelRatio > 2 ? 2 : window.devicePixelRatio
+                }}
+            >
+                <PerspectiveCamera
+                    makeDefault fov={30}
+                    near={0.1}
+                    far={5000}
+                    position={[0, 0, 2]}
+                />
 
-                    <directionalLight
-                        color={'#ffffff'}
-                        position={initProps.pos}
-                        intensity={initProps.intensity}
-                        castShadow={true}
-                        shadow-mapSize-width={1024}
-                        shadow-mapSize-height={1024}
-                        shadow-bias={-0.005}
-                        shadow-radius={2}
-                    />
+                <directionalLight
+                    color={'#ffffff'}
+                    position={initProps.pos}
+                    intensity={initProps.intensity}
+                    castShadow={true}
+                />
 
-                    <primitive ref={earthRef} object={Earth} scale={1} position={[0, -67.5, -135]} />
+                <primitive ref={earthRef} object={Earth} scale={0.15} position={[0, -140, -270]} />
 
-                    <EffectComposer depthBuffer>
-                        <BrightnessContrast brightness={0} contrast={0.5} />
-                    </EffectComposer>
-                </Canvas>
-            </section>
+                <EffectComposer depthBuffer>
+                    <BrightnessContrast brightness={0.2} contrast={0.5} />
+                    <Bloom intensity={0.25} luminanceSmoothing={0.9} />
+                    <Vignette offset={0.5} darkness={0.75} />
+                </EffectComposer>
+            </Canvas>
         </React.Fragment>
     )
 }
